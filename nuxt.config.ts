1a528cd6debeaef5edec6924ae1d2378adc47587
Ese19-stack <eseoheogbe@gmail.com>
2022-12-22 19:15:46 +0100

    scroll reveal added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,11 @@ export default defineNuxtConfig({
             link: [      
                 { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }    
             ],
+            script: [
+                {
+                    src: 'js/scrollreveal.min.js'
+                },
+            ],
         }
     },
       
